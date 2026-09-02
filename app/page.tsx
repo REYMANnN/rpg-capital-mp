2d128ad140f6cb61a309235ea775f3ee09740c34
REYMANnN <148915138+REYMANnN@users.noreply.github.com>
2026-09-02 19:21:43 -0300

    feat: route public entry to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect(process.env.BALCAO_ACCOUNTS_ENFORCED === 'true' ? '/login' : '/inventory-v1')
+  redirect('/home')
 }
